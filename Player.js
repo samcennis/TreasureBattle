ea67dfac70f76855d8dfdf4c9b4793ed37464305
djeinck <deinck@up.com>
2016-04-05 18:19:44 -0500

    fixed bug with shortest distance and edited the placement of the red marker label

--- Player.js
+++ Player.js
@@ -1,97 +1,98 @@
 var Player = function (playerId) {
-    var id = playerId
-        , tokens = []
-        , found = 0
-        , lost = 0
-        , guesses = 0;
+  var id = playerId
+    , tokens = []
+    , found = 0
+    , lost = 0
+    , guesses = 0;
 
-    // Getters and setters
-    var getId = function () {
-        return id;
-    };
+  // Getters and setters
+  var getId = function () {
+    return id;
+  };
 
-    var getFound = function () {
-        return found;
-    };
+  var getFound = function () {
+    return found;
+  };
 
-    var getLost = function () {
-        return lost;
-    };
+  var getLost = function () {
+    return lost;
+  };
 
-    var getGuesses = function () {
-        return guesses;
-    };
+  var getGuesses = function () {
+    return guesses;
+  };
 
-    var incGuesses = function () {
-        guesses++;
-    };
+  var incGuesses = function () {
+    guesses++;
+  };
 
-    var getDistanceFromClosestToken = function (otherLat, otherLng) {
-        var shortest = 99999.99;
-        for (var i = 0; i < tokens.length; i++) {
-            var thisDistance = tokens[i].getDist(otherLat, otherLng);
-            if (thisDistance < shortest)
-                shortest = thisDistance;
-        }
-        return shortest;
-    };
-
-    var getDistancesFromNonFoundTokens = function (otherLat, otherLng) {
-        var arr = [];
-        for (i = 0; i < tokens.length; i++) {
-            if (tokens[i].found != true) {
-                arr.push({
-                    token_index: i
-                    , distance: tokens[i].getDist(otherLat, otherLng)
-                });
-            }
-        }
-        return arr;
+  var getDistanceFromClosestToken = function (otherLat, otherLng) {
+    var shortest = 99999.99;
+    for (var i = 0; i < tokens.length; i++) {
+      var thisDistance = tokens[i].getDist(otherLat, otherLng);
+      if (thisDistance < shortest) {
+        shortest = thisDistance;
+      }
     }
+    return shortest;
+  };
 
-    var lostToken = function (index) {
-        lost += 1;
-        tokens[index].found = true;
-    };
+  var getDistancesFromNonFoundTokens = function (otherLat, otherLng) {
+    var arr = [];
+    for (i = 0; i < tokens.length; i++) {
+      if (tokens[i].found != true) {
+        arr.push({
+          token_index: i
+          , distance: tokens[i].getDist(otherLat, otherLng)
+        });
+      }
+    }
+    return arr;
+  }
 
-    var incFound = function () {
-        found += 1;
-    };
+  var lostToken = function (index) {
+    lost += 1;
+    tokens[index].found = true;
+  };
 
-    var addToken = function (token) {
-        tokens.push(token);
-    };
+  var incFound = function () {
+    found += 1;
+  };
 
-    var getStats = function () {
-        return {
-            id: id
-            , found: found
-            , lost: lost
-            , guesses: guesses
-        };
-    };
+  var addToken = function (token) {
+    tokens.push(token);
+  };
 
-    var getTokenByIndex = function (index) {
-
-        //console.log("HERE IS THE TOKEN BY INDEX lat: " + tokens[index].getLat());
-        return tokens[index];
-    };
-
-
-    // Define which variables and methods can be accessed
+  var getStats = function () {
     return {
-        getId: getId
-        , getFound: getFound
-        , getLost: getLost
-        , incFound: incFound
-        , addToken: addToken
-        , lostToken: lostToken
-        , getTokenByIndex: getTokenByIndex
-        , getStats: getStats
-        , incGuesses: incGuesses
-        , getDistanceFromClosestToken: getDistanceFromClosestToken
-        , getDistancesFromNonFoundTokens: getDistancesFromNonFoundTokens
-    }
+      id: id
+      , found: found
+      , lost: lost
+      , guesses: guesses
+    };
+  };
+
+  var getTokenByIndex = function (index) {
+
+    //console.log("HERE IS THE TOKEN BY INDEX lat: " + tokens[index].getLat());
+    return tokens[index];
+  };
+
+
+  // Define which variables and methods can be accessed
+  return {
+    getId: getId
+    , getFound: getFound
+    , getLost: getLost
+    , incFound: incFound
+    , addToken: addToken
+    , lostToken: lostToken
+    , getTokenByIndex: getTokenByIndex
+    , getStats: getStats
+    , incGuesses: incGuesses
+    , getDistanceFromClosestToken: getDistanceFromClosestToken
+    , getDistancesFromNonFoundTokens: getDistancesFromNonFoundTokens
+  }
 };
 
 // Export the Player class so you can use it in
